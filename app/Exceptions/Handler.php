1ace1feeaf3ab504b2f8a790f610fbb7ee6ab5fa
narwinoks <narnowin195@gmail.com>
2023-05-17 08:07:54 +0700

    update folder stuctur

--- app/Exceptions/Handler.php
+++ app/Exceptions/Handler.php
@@ -2,13 +2,37 @@
 
 namespace App\Exceptions;
 
+use Illuminate\Auth\AuthenticationException;
+use Illuminate\Database\Eloquent\ModelNotFoundException;
 use Illuminate\Foundation\Exceptions\Handler as ExceptionHandler;
+use Illuminate\Http\JsonResponse;
 use Throwable;
+use Illuminate\Http\Response;
+use Symfony\Component\HttpKernel\Exception\MethodNotAllowedHttpException;
+use Symfony\Component\HttpKernel\Exception\NotFoundHttpException;
 
 class Handler extends ExceptionHandler
 {
     /**
-     * The list of the inputs that are never flashed to the session on validation exceptions.
+     * A list of exception types with their corresponding custom log levels.
+     *
+     * @var array<class-string<\Throwable>, \Psr\Log\LogLevel::*>
+     */
+    protected $levels = [
+        //
+    ];
+
+    /**
+     * A list of the exception types that are not reported.
+     *
+     * @var array<int, class-string<\Throwable>>
+     */
+    protected $dontReport = [
+        //
+    ];
+
+    /**
+     * A list of the inputs that are never flashed to the session on validation exceptions.
      *
      * @var array<int, string>
      */
@@ -20,11 +44,54 @@ class Handler extends ExceptionHandler
 
     /**
      * Register the exception handling callbacks for the application.
+     *
+     * @return void
      */
-    public function register(): void
+    public function register()
     {
         $this->reportable(function (Throwable $e) {
             //
         });
+    }
+
+    public function render($request, Throwable $exception)
+    {
+        if ($exception instanceof MethodNotAllowedHttpException) {
+            return response()->json([
+                'success' => false,
+                'message' => 'Method is not allowed for the requested route',
+            ], 405);
+        } else if ($exception instanceof NotFoundHttpException) {
+            return response()->json([
+                'success' => false,
+                'message' => 'Not Found!'
+            ], 404);
+        }
+        return $this->getJsonResponse($exception);
+        // return parent::render($request, $exception);
+    }
+
+    protected function unauthenticated($request, \Illuminate\Auth\AuthenticationException $exception)
+    {
+        return response()->json([
+            'success' => false,
+            'message' => $exception->getMessage()
+        ], 401);
+    }
+
+    // return parent::render($request, $exception);
+    protected function getJsonResponse(Throwable $exception): JsonResponse
+    {
+        $responseData = [
+            'message' => $exception->getMessage(),
+            'code' => $exception->getCode(),
+        ];
+
+        // Jika Anda ingin menambahkan informasi tambahan ke respons JSON,
+        // Anda dapat menambahkan kunci-kunci lain ke $responseData sesuai kebutuhan.
+
+        $statusCode = $this->isHttpException($exception) ? $exception->getStatusCode() : Response::HTTP_INTERNAL_SERVER_ERROR;
+
+        return response()->json($responseData, $statusCode);
     }
 }
